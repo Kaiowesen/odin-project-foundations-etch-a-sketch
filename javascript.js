75b3fed5169178bacf0ecca22cf92b9dc0e66f97
Kai Hansen <kaiowesen@gmail.com>
2024-01-25 19:54:57 +0100

    javascript working. Need to fix styling

--- javascript.js
+++ javascript.js
@@ -1,12 +1,32 @@
 let board = document.querySelector('.pad')
-board.style.gridTemplateColumns = 'repeat(16, 1fr)'
-board.style.gridTemplateRows = 'repeat(16, 1fr)'
+let gridSize = 16;
 
-for (var i=0; i < 256; i++) {
-let div = document.createElement('div');
-div.className = "grid-element"
-div.style.backgroundColor = 'beige  '
-board.appendChild(div)
+function resetGridSize() {
+    document.querySelectorAll(".grid-element").forEach(x => x.remove());
+
+    let newGridSizeValue = document.getElementById("box-size").value
+
+    setGridSize(newGridSizeValue);
 }
+
+
+function setGridSize(newGridSize = 16) {
+
+    board.style.gridTemplateColumns = `repeat(${newGridSize}, 1fr)`
+    board.style.gridTemplateRows = `repeat(${newGridSize}, 1fr)`
+
+    for (var i = 0; i < 256; i++) {
+        let div = document.createElement('div');
+        div.className = "grid-element";
+        div.style.backgroundColor = 'beige';
+
+        div.addEventListener("mouseover", (event) => {
+            event.target.style.backgroundColor = "black"
+        });
+
+        board.appendChild(div)
+    }
+}
+setGridSize();
 
 
